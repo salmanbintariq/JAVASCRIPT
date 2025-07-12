ee98c87ccd5707b631d1165d49e662fdb1f2ce57
Salman Tariq <salmantariq8018@gmail.com>
2025-07-12 12:55:08 +0500

    Day 1: Header and theme toggle added

---
+++ intermediate_projects/3.smart-task-managaer/script.js
@@ -0,0 +1,12 @@
+const themeBtn = document.getElementById('themeToggle');
+
+themeBtn.addEventListener('click',()=>{
+  document.body.classList.toggle('dark');
+
+  if(document.body.classList.contains('dark')){
+    themeBtn.textContent = "☀️ Light Mode";
+  }else{
+    themeBtn.textContent = "🌙 Dark Mode";
+  };
+
+});
